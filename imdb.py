b05c4e7aa37f8c3ed1d85dd36f13ca7789d632af
Dreyforever <109780385+Dreyforever@users.noreply.github.com>
2023-08-17 02:11:51 +0530

    imdb.py

--- imdb.py
+++ imdb.py
@@ -1,3 +1,4 @@
+# Import required libraries
 import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 from transformers import Trainer, TrainingArguments
@@ -15,33 +16,32 @@ tokenizer = DistilBertTokenizer.from_pretrained(model_name)
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True)
 
+# Apply tokenization function to the dataset in batches
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-# Define training arguments for fine-tuning with checkpoints
+# Define training arguments for fine-tuning
 training_args = TrainingArguments(
-    output_dir="./sentiment_model",
-    evaluation_strategy="steps",
-    save_strategy="steps",
-    learning_rate=1e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=3,    # Increase number of epochs for more checkpoints
-    eval_steps=500,
-    save_steps=500,
-    save_total_limit=3,    # Limit the number of checkpoints to save
-    load_best_model_at_end=True,  # Load the best model when training ends
+    output_dir="./sentiment_model",         # Directory to save model checkpoints and logs
+    evaluation_strategy="steps",            # Evaluate during training at certain steps
+    save_strategy="steps",                  # Save model during training at certain steps
+    learning_rate=1e-5,                     # Learning rate for optimizer
+    per_device_train_batch_size=8,          # Batch size for training
+    per_device_eval_batch_size=8,           # Batch size for evaluation
+    num_train_epochs=1,                     # Number of training epochs (1 epoch here) to lower the training time
+    eval_steps=500,                         # Evaluate every 500 steps
+    save_steps=500,                         # Save model every 500 steps
 )
 
-# Create a Trainer instance with checkpoints
+# Create a Trainer instance for training the model
 trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["test"],
+    model=model,                            # The model to be trained
+    args=training_args,                     # Training arguments defined above
+    train_dataset=tokenized_datasets["train"],  # Training dataset after tokenization
+    eval_dataset=tokenized_datasets["test"],    # Evaluation dataset after tokenization
 )
 
 # Train the model using the Trainer instance
 trainer.train()
 
-# Save the final model
+# Save the fine-tuned model after training
 model.save_pretrained("./fine_tuned_model")
