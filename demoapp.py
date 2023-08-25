a8f7f57f53c7d027590f31da7fb0366911afc296
Dreyforever <109780385+Dreyforever@users.noreply.github.com>
2023-08-25 13:32:23 +0530

    demoapp.py

--- demoapp.py
+++ demoapp.py
@@ -1,38 +1,34 @@
 import gradio as gr
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 import torch
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
-# Load fine-tuned sentiment analysis model
-model_path = "./fine_tuned_model"  # Path to the saved fine-tuned model
-model = DistilBertForSequenceClassification.from_pretrained(model_path)  # Load the model
-tokenizer = DistilBertTokenizer.from_pretrained(model_path)  # Load the tokenizer
+# Load the pre-trained DistilBERT model and tokenizer
+model_name = "distilbert-base-uncased"
+model = DistilBertForSequenceClassification.from_pretrained(model_name, num_labels=2)
+tokenizer = DistilBertTokenizer.from_pretrained(model_name)
 
+# Loading the checkpoint model from fine-tuning
+checkpoint_path = "./sentiment_model/checkpoint-1500"  # Update this path accordingly
+checkpoint_model = DistilBertForSequenceClassification.from_pretrained(checkpoint_path)
 
-# Define a function to predict sentiment from text
+# Defining a function to make predictions using the checkpoint model
 def predict_sentiment(text):
-    # Tokenize the input text and convert to PyTorch tensor
-    inputs = tokenizer(text, padding="max_length", truncation=True, return_tensors="pt")
-
-    # Make a prediction using the model
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
     with torch.no_grad():
-        outputs = model(**inputs)
+        outputs = checkpoint_model(**inputs)
         logits = outputs.logits
-
-    # Determine the predicted sentiment class and convert to a sentiment label
-    predicted_class = torch.argmax(logits, dim=1).item()
-    sentiment = "positive" if predicted_class == 1 else "negative"
-
+        predicted_class = torch.argmax(logits, dim=1).item()
+        sentiment = "Positive" if predicted_class == 1 else "Negative"
     return sentiment
 
-
-# Create a Gradio interface for sentiment prediction
+# Create a Gradio interface
 iface = gr.Interface(
-    fn = predict_sentiment,
-    inputs = "text",
-    outputs = "text",
-    title = "Sentiment Analysis Of IMDb Reviews",
-    description = "Enter a text and get sentiment prediction.",
+    fn=predict_sentiment,
+    inputs=gr.inputs.Textbox(),
+    outputs=gr.outputs.Textbox(),
+    title="IMDb Reviews Sentiment Analysis",
+    description="Enter a review and get sentiment prediction using the model.",
 )
 
-# Launching the Gradio interface
+# Launch the Gradio interface
 iface.launch()
